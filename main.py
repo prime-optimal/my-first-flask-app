9fcc0c9f64312cd31ff1330b5dcf3d8351f0ee3b
optimal prime <prime.optimal@pm.me>
2022-07-14 23:49:44 -0700

    Got the player working with the example files.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas
+from streamlit_player import st_player
 
 data = {
     'Series_1': [1, 3, 4, 5, 7],
@@ -14,5 +15,8 @@ st.title('This is Streamlit, baby')
 st.subheader('Introducing Tools, which is LIT')
 st.write('These streamlit apps should be OBS overlays and plugins')
 
-st.write(df)
-st.line_chart(df)
+# Embed a youtube video
+st_player("https://youtu.be/CmSKVW1v0xM")
+
+# Embed a music from SoundCloud
+st_player("https://soundcloud.com/imaginedragons/demons")
